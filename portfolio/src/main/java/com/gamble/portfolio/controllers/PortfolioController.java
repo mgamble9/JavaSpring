2399534557f160942f0af4f5f6a9321e4b0c8f56
Mike Gamble <mgamble9@yahoo.com>
2017-09-12 17:06:20 -0700

    Portfolio assignment: Spring, Static Files, Routing

---
+++ portfolio/src/main/java/com/gamble/portfolio/controllers/PortfolioController.java
@@ -0,0 +1,23 @@
+package com.gamble.portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PortfolioController {
+	@RequestMapping("/")
+	public String index() {
+		return "forward:/index.html";
+	}
+	
+	@RequestMapping("/metoo")
+	public String me() {
+		return "forward:/me.html";
+	}
+
+	@RequestMapping("/portfolio")
+	public String portfolio() {
+		return "forward:/portfolio.html";
+	}
+
+}
